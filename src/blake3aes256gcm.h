64a25c80325c957faaba848398bbb1e6ebf3ec6a
thehxdev <hossein.khosravi.ce@gmail.com>
2025-06-18 00:42:32 +0330

    removed unnecessary responsibilities from library
    Signed-off-by: thehxdev <hossein.khosravi.ce@gmail.com>

--- src/blake3aes256gcm.h
+++ src/blake3aes256gcm.h
@@ -9,49 +9,35 @@
 #include <wolfssl/wolfcrypt/random.h>
 #include <wolfssl/wolfcrypt/aes.h>
 
-#define AES256_IV_LENGTH (12)
+#define AES256_IV_SIZE (12)
 
-#define AES256_KEY_LENGTH (32)
-#define AES256_BASE64_KEY_LENGTH (44)
+#define AES256_KEY_SIZE (32)
+#define AES256_BASE64_KEY_SIZE (44)
 
-#define AES256_SALT_LENGTH AES256_KEY_LENGTH
-#define AES256_AUTH_TAG_LENGTH (16)
+#define AES256_SALT_SIZE AES256_KEY_SIZE
+#define AES256_AUTH_TAG_SIZE (16)
 
 typedef struct blake3_aes256gcm {
     Aes aes;
-    byte key_with_salt[AES256_KEY_LENGTH+AES256_SALT_LENGTH];
     byte derived_key[BLAKE3_OUT_LEN];
-    byte iv[AES256_IV_LENGTH];
-    WC_RNG rng;
+    byte iv[AES256_IV_SIZE];
 } Blake3Aes256Gcm_t;
 
-enum {
-    AES256_KEY_TYPE_PLAIN,
-    AES256_KEY_TYPE_BASE64,
-};
+int blake3aes256gcm_zero_iv(byte *iv_out);
 
+int blake3aes256gcm_gen_derived_key(const byte *key, const byte *salt, byte *derived_key_out);
 
-int blake3aes256gcm_set_key(Blake3Aes256Gcm_t *self, const byte *key, size_t keytype);
-
-int blake3aes256gcm_set_salt(Blake3Aes256Gcm_t *self);
-
-int blake3aes256gcm_set_iv(Blake3Aes256Gcm_t *self);
-
-int blake3aes256gcm_set_derived_key(Blake3Aes256Gcm_t *self);
-
-int blake3aes256gcm_init(Blake3Aes256Gcm_t *self, const byte *key, size_t keytype);
+int blake3aes256gcm_init(Blake3Aes256Gcm_t *self, const byte *key, const byte *salt);
 
 void blake3aes256gcm_encrypt(Blake3Aes256Gcm_t *self, byte *out,
                              const byte *in, size_t in_size,
-                             byte *out_iv,
-                             byte *auth_tag, size_t auth_tag_size,
+                             byte *out_iv, byte *auth_tag,
                              const byte *auth_in, size_t auth_in_size);
 
 int blake3aes256gcm_decrypt(Blake3Aes256Gcm_t *self, byte *out,
-                             const byte *in, size_t in_size,
-                             const byte *iv,
-                             const byte *auth_tag, size_t auth_tag_size,
-                             const byte *auth_in, size_t auth_in_size);
+                            const byte *in, size_t in_size,
+                            const byte *iv, const byte *auth_tag,
+                            const byte *auth_in, size_t auth_in_size);
 
 void blake3aes256gcm_destroy(Blake3Aes256Gcm_t *self);
 
--- src/main.c
+++ src/main.c
@@ -2,7 +2,13 @@
 #include <unistd.h>
 #include <assert.h>
 #include <string.h>
+
+#include <wolfssl/options.h>
+#include <wolfssl/wolfcrypt/types.h>
+#include <wolfssl/wolfcrypt/random.h>
+
 #include "blake3aes256gcm.h"
+#include "base64.h"
 
 #define INPUT "This is Blake3-AES-256-GCM!"
 
@@ -10,12 +16,24 @@ int main(int argc, char *argv[]) {
     if (argc < 2)
         return 1;
 
-    const size_t input_len = strlen(INPUT);
+    WC_RNG rng;
     Blake3Aes256Gcm_t aes;
+    byte salt[AES256_SALT_SIZE];
+    byte key[AES256_KEY_SIZE];
+    size_t decoded_len;
+    const size_t input_len = strlen(INPUT);
 
+    wc_InitRng(&rng);
     printf("Base64 Key:\n%s\n\n", argv[1]);
 
-    blake3aes256gcm_init(&aes, (byte*)argv[1], AES256_KEY_TYPE_BASE64);
+    // Generate random salt
+    wc_RNG_GenerateBlock(&rng, salt, sizeof(salt));
+
+    // Decode base64 encoded key
+    decoded_len = base64_decode(argv[1], AES256_BASE64_KEY_SIZE, (char*)key);
+    assert(decoded_len == AES256_KEY_SIZE);
+
+    blake3aes256gcm_init(&aes, key, salt);
 
     byte input[AES_BLOCK_SIZE*3];
     memset(input, 0, sizeof(input));
@@ -24,34 +42,35 @@ int main(int argc, char *argv[]) {
     byte cipher[sizeof(input)];
     memset(cipher, 0, sizeof(cipher));
 
-    byte auth_tag[AES256_AUTH_TAG_LENGTH];
-    byte enc_iv[AES256_IV_LENGTH];
+    byte auth_tag[AES256_AUTH_TAG_SIZE];
+    byte enc_iv[AES256_IV_SIZE];
 
-    blake3aes256gcm_encrypt(&aes, cipher, input, input_len, enc_iv, auth_tag, sizeof(auth_tag), NULL, 0);
+    blake3aes256gcm_encrypt(&aes, cipher, input, input_len, enc_iv, auth_tag, NULL, 0);
 
     size_t i;
     ssize_t j;
-    printf("Encrypted data:\n");
+    printf("Encrypted data (HEX):\n");
     for (i = 0; i < input_len; i++)
         printf("%02x", cipher[i]);
 
-    printf("\n\nEncryption IV:\n");
+    printf("\n\nEncryption IV (HEX):\n");
     for (j = sizeof(enc_iv)-1; j >= 0; j--)
         printf("%02x", enc_iv[j]);
 
-    printf("\n\nAuth tag:\n");
+    printf("\n\nAuth tag (HEX):\n");
     for (i = 0; i < sizeof(auth_tag); i++)
         printf("%02x", auth_tag[i]);
 
     memset(input, 0, sizeof(input));
-    blake3aes256gcm_decrypt(&aes, input, cipher, input_len, enc_iv, auth_tag, sizeof(auth_tag), NULL, 0);
-    printf("\n\nDecrypted data:\n%s\n", input);
+    blake3aes256gcm_decrypt(&aes, input, cipher, input_len, enc_iv, auth_tag, NULL, 0);
+    printf("\n\nDecrypted data (HEX):\n%s\n", input);
 
-    printf("\nIV after encrypt and decrypt:\n");
+    printf("\nIV after encrypt and decrypt (HEX):\n");
     for (j = sizeof(enc_iv)-1; j >= 0; j--)
         printf("%02x", aes.iv[j]);
     putchar('\n');
 
     blake3aes256gcm_destroy(&aes);
+    wc_FreeRng(&rng);
     return 0;
 }
